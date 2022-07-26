2265686b7ad036b935ff35fde32a0b678fbb71d3
Inesseni <ineshilz@yahoo.de>
2022-07-26 23:52:56 +0200

    Init commit
    Yogaapp UI only

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import reactDom from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import YogaPose1 from './assets/YogaPose1.jpg'
+import YogaPose2 from './assets/YogaPose2.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Today tasks*/}
+      <Text style={styles.AppName}>SUPER AWESOME YOGA APP!</Text>
+
+
+      <View style={styles.nextPoseWrapper}>
+        <View>
+          <Text style={styles.NextPoseText}>Next:</Text>
+          <Text style={styles.NextPoseName}>Warrior</Text>
+        </View>
+
+        <View style={styles.NextImageThumbnail}>
+          <img src={YogaPose2} alt="Warrior" />
+        </View>
+      </View>
+
+      <View style={styles.YogastretchWrapper}>
+      <Text style={styles.sectionTitle}>Do this Yoga pose for 3 Minutes:</Text>
+        <Text style={styles.NextPoseName}>Downward facing dog</Text>
+        <View style={styles.CurrentImage}>
+          <img src={YogaPose1} alt="DownwardDog" />
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +37,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f28d79',
+    padding: 20,
   },
+  AppName: {
+    fontSize: 40,
+    fontWeight: 1000,
+    textAlign: 'center',
+    marginBottom: 30,
+    color: '#ffffff',
+  },
+  CurrentImage: {
+    minWidth: 300,
+    marginTop: 10,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  nextPoseWrapper: {
+    height: 75,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  NextImageThumbnail: {
+    maxWidth: 100,
+    flex: 1,
+  },
+  NextPoseText: {
+    flex: 1,
+    textAlign: 'right',
+
+  },
+  NextPoseName: {
+    fontSize: 30,
+    fontWeight: 600,
+  },
+  YogastretchWrapper: {
+    paddingTop: 40,
+
+  },
+
 });
+
+
+/*Fragen:
+1.
+wie kann ich ein bild automatisch an den wrapper anpassen? 
+zB das Thumbnail für nächste Pose, jetzt ist es hardcoded
+
+2.
+
+
+*/
